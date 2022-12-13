22696bce0e8c74b10a8158e116905b7eb7cea0b2
Muhammad Daniyal Javed <101814302+daniyal9999@users.noreply.github.com>
2022-12-13 10:40:53 +0500

    rem unecessary nod mods

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import ImageUpload from './create_post/ImageUpload';
 
 function App() {
+  const [image,setImage] = useState('');
+  
+  function handleOpenWidget(e){
+    e.preventDefault();
+    var myWidget = window.cloudinary.createUploadWidget({
+        cloudName: 'dfmxbcddb', 
+        uploadPreset: 'krdozupg'}, (error, result) => { 
+          if (!error && result && result.event === "success") { 
+            console.log(result.info.url)
+            // setImages(prev => [...prev, { url: result.info.url, public_id: result.info.public_id}]); 
+            setImage(result.info.url); 
+          }
+        }
+      )
+      myWidget.open();
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault()
+
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          repo pulled <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ImageUpload/>
+      daisdji
+      App.js
+      <form className="create" onSubmit={handleSubmit}>  
+        <button id='upload-widget' className='cloudinary-button' onClick={handleOpenWidget}>Upload Image</button> 
+        <button>Add Recipe</button>
+      </form>
+      <img src={image} alt="image" />
+   </div>
   );
 }
 
