3f5c896af30bb90bd6f478a322f62a567d75ebbe
Abdul Rehman Aziz <abdulrehmanx911@gmail.com>
2022-12-16 03:04:09 +0500

    Completed routes and rest of server

--- artficial.api/app.js
+++ artficial.api/app.js
@@ -1,41 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config()
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
+const mongoose = require('mongoose')
+const artworkRoutes = require('./routes/artworks')
+const userRoutes = require('./routes/users')
+const artistPresets= require('./routes/artistPresets')
+const categories = require('./routes/categories')
+const galleries = require('./routes/galleries')
+const promotedArtworks = require('./routes/promotedArtworks')
+const reportArtworks = require('./routes/reportArtworks')
+const themePresets = require('./routes/themePresets')
 
-var app = express();
+const app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//middlewares
+app.use(express.json())
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next()
+})
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api/artworks',artworkRoutes)
+app.use('/api/users',userRoutes)
+app.use('/api/artistPresets',artistPresets)
+app.use('/api/categories',categories)
+app.use('/api/galleries',galleries)
+app.use('/api/promotedArtworks',promotedArtworks)
+app.use('/api/reportArtworks',reportArtworks)
+app.use('/api/themePresets',themePresets)
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+//Connecting to mongo
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log("listening on 4000")
+    })
+})
+.catch((err)=>{console.log(err)})
