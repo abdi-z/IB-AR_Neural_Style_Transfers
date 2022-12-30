bb4a360aa02d1fb00091ca04e0cf54df2a82bdd1
Abdul Rehman Aziz <abdulrehmanx911@gmail.com>
2022-12-30 22:15:01 +0500

    Ini:Editing Homepage

--- artficial.webapp/src/components/Essentials/Home/Home.js
+++ artficial.webapp/src/components/Essentials/Home/Home.js
@@ -1,5 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { HStack, VStack, Stack } from "@chakra-ui/react";
+import './Home.css'
+
 export default function Home() {
-  return <>Nothing</>;
+
+  return <>
+  <VStack>
+    
+  </VStack>
+  </>;
 }
