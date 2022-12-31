65bd16a20662d39d47d091c5b6c20557baa63f8a
Abdul Rehman Aziz <abdulrehmanx911@gmail.com>
2022-12-31 12:56:45 +0500

    Fix: Import Error

--- artficial.webapp/src/components/Essentials/Home/Home.js
+++ artficial.webapp/src/components/Essentials/Home/Home.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { HStack, VStack, Stack } from "@chakra-ui/react";
-import './Home.css'
 
 export default function Home() {
-
-  return <>
-  <VStack>
-    
-  </VStack>
-  </>;
+  return (
+    <>
+      <VStack></VStack>
+    </>
+  );
 }
