2302fa3efdf53630f3a93e0994bce79f061ba243
Abdul Rehman Aziz <abdulrehmanx911@gmail.com>
2023-01-02 13:42:46 +0500

    Feat: Added loader for all artworks

--- artficial.webapp/src/components/Artwork/AllArtworks/AllArtworks.js
+++ artficial.webapp/src/components/Artwork/AllArtworks/AllArtworks.js
@@ -1,24 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useArtworksContext } from "../../../hooks/useArtworksContext";
-import { Box, Button, SimpleGrid } from "@chakra-ui/react";
+import { Box, Button, Heading, SimpleGrid } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { Center } from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/react";
 import { Flex, Spacer } from "@chakra-ui/react";
 // components
 import SingleArtwork from "../SingleArtwork/SingleArtwork";
 
-
 const AllArtworks = () => {
   const { artworks, dispatch } = useArtworksContext();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetchArtworks = async () => {
+      setLoading(true);
       const response = await fetch("/api/artworks");
       const json = await response.json();
 
       if (response.ok) {
         dispatch({ type: "SET_ARTWORKS", payload: json });
+      } else {
+        setError(true);
       }
+
+      setLoading(false);
     };
 
     fetchArtworks();
@@ -26,23 +33,29 @@ const AllArtworks = () => {
 
   return (
     <>
-      All Artworks here
+      <Heading>
+        Welcome to ARTficial
+      </Heading>
       <SimpleGrid
         // m={1}
         minChildWidth="240px"
         spacing="60px"
         bg="grey.400"
         style={{ position: "relative" }}>
-        
-          {artworks &&
-            artworks.map((Artwork) => (
-              <SingleArtwork
-                key={Artwork._id}
-                artwork={Artwork}
-                textAlign={"center"}
-              />
-            ))}
-        
+        {loading && (
+          <Center>
+            <Spinner color="red.500" />
+          </Center>
+        )}
+        {error && <Center>Something went wrong</Center>}
+        {artworks &&
+          artworks.map((Artwork) => (
+            <SingleArtwork
+              key={Artwork._id}
+              artwork={Artwork}
+              textAlign={"center"}
+            />
+          ))}
       </SimpleGrid>
     </>
   );
