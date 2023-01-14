34f5f74b290baa1f26d7f3ed29fc658840d0bda8
Abdul Rehman Aziz <abdulrehmanx911@gmail.com>
2023-01-14 16:09:18 +0500

    Fix: Added CORs policy for main server

--- artficial.api/app.js
+++ artficial.api/app.js
@@ -1,43 +1,50 @@
-require('dotenv').config()
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+const artworkRoutes = require("./routes/artworks");
+const userRoutes = require("./routes/users");
+const artistPresets = require("./routes/artistPresets");
+const categories = require("./routes/categories");
+const galleries = require("./routes/galleries");
+const promotedArtworks = require("./routes/promotedArtworks");
+const reportArtworks = require("./routes/reportArtworks");
+const themePresets = require("./routes/themePresets");
+const index = require("./routes/index");
 
-const express = require('express');
-const mongoose = require('mongoose')
-const artworkRoutes = require('./routes/artworks')
-const userRoutes = require('./routes/users')
-const artistPresets= require('./routes/artistPresets')
-const categories = require('./routes/categories')
-const galleries = require('./routes/galleries')
-const promotedArtworks = require('./routes/promotedArtworks')
-const reportArtworks = require('./routes/reportArtworks')
-const themePresets = require('./routes/themePresets')
-const index=require('./routes/index')
-
-const app = express()
+const app = express();
 
 //middlewares
-app.use(express.json())
-app.use((req,res,next)=>{
-    console.log(req.path,req.method)
-    next()
-})
 
-app.use('/',index)
-app.use('/api/v1/artworks',artworkRoutes)
-app.use('/api/v1/users',userRoutes)
-app.use('/api/v1/artistPresets',artistPresets)
-app.use('/api/v1/categories',categories)
-app.use('/api/v1/galleries',galleries)
-app.use('/api/v1/promotedArtworks',promotedArtworks)
-app.use('/api/v1/reportArtworks',reportArtworks)
-app.use('/api/v1/themePresets',themePresets)
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
-
+app.use("/", index);
+app.use("/api/v1/artworks", artworkRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/artistPresets", artistPresets);
+app.use("/api/v1/categories", categories);
+app.use("/api/v1/galleries", galleries);
+app.use("/api/v1/promotedArtworks", promotedArtworks);
+app.use("/api/v1/reportArtworks", reportArtworks);
+app.use("/api/v1/themePresets", themePresets);
 
 //Connecting to mongo
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>{
-    app.listen(process.env.PORT,()=>{
-        console.log("live: listening on 4000")
-    })
-})
-.catch((err)=>{console.log(err)})
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("live: listening on 4000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
